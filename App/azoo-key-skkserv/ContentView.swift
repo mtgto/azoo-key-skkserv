4b64e45698ad73f4e6c180429b6ef3cfce41bdf5
Satoshi Goto <hogerappa@gmail.com>
2025-07-11 00:05:42 +0900

    chore: add xcodeproj

---
+++ App/azoo-key-skkserv/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  azoo-key-skkserv
+//
+//  Created by mtgto on 2025/07/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ App/azoo-key-skkserv/azoo_key_skkservApp.swift
@@ -0,0 +1,17 @@
+//
+//  azoo_key_skkservApp.swift
+//  azoo-key-skkserv
+//
+//  Created by mtgto on 2025/07/11.
+//
+
+import SwiftUI
+
+@main
+struct azoo_key_skkservApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
